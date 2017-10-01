513df99327bbb6be69710fc3cbf77a6c124c39e4
Gharbi Mohammed <mgharbik@gmail.com>
2017-10-01 11:25:47 +0200

    stateless funcional components

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,10 +1,15 @@
-class Appointment extends React.Component {
-  render () {
-    return (
-      <div className='appointment'>
-        <h3>{this.props.appointment.title}</h3>
-        <p>{formatDate(this.props.appointment.appt_time)}</p>
-      </div>
-    )
-  }
-}
+const Appointment = ({appointment}) =>
+  <div className='appointment'>
+    <h3>{appointment.title}</h3>
+    <p>{formatDate(appointment.appt_time)}</p>
+  </div>
+
+//// Second way
+// const Appointment = (props) => {
+//   return (
+//     <div className='appointment'>
+//       <h3>{props.appointment.title}</h3>
+//       <p>{formatDate(props.appointment.appt_time)}</p>
+//     </div>
+//   )
+// }
