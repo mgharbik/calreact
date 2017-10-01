8bd926f244aad2d3777118c24a99fd421f4c9fd5
Gharbi Mohammed <mgharbik@gmail.com>
2017-10-01 10:57:52 +0200

    migrating to es6 classes

--- app/assets/javascripts/components/appointment_form.es6.jsx
+++ app/assets/javascripts/components/appointment_form.es6.jsx
@@ -1,42 +1,41 @@
-var AppointmentForm = React.createClass({
-  handleChange: function(e) {
-    var name = e.target.name;
+class AppointmentForm extends React.Component {
+  handleChange (e) {
+    const name = e.target.name;
     obj = {};
     obj[name] = e.target.value;
     this.props.onUserInput(obj);
-  },
+  }
 
-  setAppointmentTime: function(e) {
-    var name = 'appt_time';
+  setAppointmentTime (e) {
+    const name = 'appt_time';
     obj = {};
     if(obj[name] = e.toDate()) {
       this.props.onUserInput(obj);
     }
-  },
+  }
 
-  handleSubmit: function(e) {
+  handleSubmit (e) {
     e.preventDefault();
     this.props.onFormSubmit()
-  },
+  }
 
-  render: function() {
-    var inputProps = {
+  render () {
+    const inputProps = {
       name: 'appt_time'
     }
     return (
       <div>
         <h3>Make a new Appointment</h3>
-        <Label label='Enter a title, date and time' />
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <input name='title' placeholder='Appointment Title'
                  value={this.props.title}
-                 onChange={this.handleChange} />
+                 onChange={this.handleChange.bind(this)} />
           <Datetime input={false} open={true} inputProps={inputProps}
                     value={this.props.appt_time}
-                    onChange={this.setAppointmentTime} />
+                    onChange={this.setAppointmentTime.bind(this)} />
           <input type='submit' value='Make Appointment' className='submit-button' />
         </form>
       </div>
     )
   }
-});
+}
--- app/assets/javascripts/components/appointments.es6.jsx
+++ app/assets/javascripts/components/appointments.es6.jsx
@@ -1,43 +1,44 @@
-var Appointments = React.createClass({
-  getInitialState: function() {
-    return {
+class Appointments extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
       appointments: this.props.appointments,
       title: 'Team standup meeting',
       appt_time: 'Tomorrow at 9am',
     }
-  },
+  }
 
-  handleUserInput: function(obj) {
+  handleUserInput (obj) {
     this.setState(obj);
-  },
+  }
 
-  handleFormSubmit: function() {
-    var appointment = {title: this.state.title, appt_time: this.state.appt_time}
+  handleFormSubmit () {
+    const appointment = {title: this.state.title, appt_time: this.state.appt_time}
     $.post('/appointments',
             {appointment: appointment})
         .done(function(data){
           this.addNewAppointment(data);
         }.bind(this));
-  },
+  }
 
-  addNewAppointment: function(appointment) {
-    var appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
+  addNewAppointment (appointment) {
+    const appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
     this.setState({
       appointments: appointments.sort(function(a,b) {
         return new Date(a.appt_time) - new Date(b.appt_time);
       })
     });
-  },
+  }
 
-  render: function() {
+  render () {
     return (
       <div>
         <AppointmentForm input_title={this.state.title}
                          input_appt_time={this.state.appt_time}
-                         onUserInput={this.handleUserInput}
-                         onFormSubmit={this.handleFormSubmit} />
+                         onUserInput={this.handleUserInput.bind(this)}
+                         onFormSubmit={this.handleFormSubmit.bind(this)} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
-});
+}
