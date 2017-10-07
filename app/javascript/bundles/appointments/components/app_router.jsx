d6fe916709400a628fc8780acdea1a8aeedabe5d
Gharbi Mohammed <mgharbik@gmail.com>
2017-10-07 23:27:03 +0200

    navigating between rotues

--- app/javascript/bundles/appointments/components/app_router.jsx
+++ app/javascript/bundles/appointments/components/app_router.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Appointments from './appointments'
 import Appointment from './appointment'
+import AppHeader from './app_header'
 
 export default (props) => {
   return (
     <Router>
       <div>
+        <AppHeader />
         <Route exact path='/' render={routeProps => (
           <Appointments {...routeProps} appointments={props.appointments} />
         )} />
